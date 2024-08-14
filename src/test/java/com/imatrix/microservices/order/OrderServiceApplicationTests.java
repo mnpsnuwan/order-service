f93af63891ce4e940275d26f9391742b342f347d
mnpsnuwan <nuwanp124@gmail.com>
2024-08-14 05:37:48 -0700

    IM:DEV:Order Service - Implementing placeOrder Unit Tests.

--- src/test/java/com/imatrix/microservices/order/OrderServiceApplicationTests.java
+++ src/test/java/com/imatrix/microservices/order/OrderServiceApplicationTests.java
@@ -1,15 +1,56 @@
 package com.imatrix.microservices.order;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
 
+	static {
+		mySQLContainer.start();
+	}
+
+	@Test
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 5
+                }
+                """;
+
+
+		var responseBodyString = RestAssured.given()
+											.contentType("application/json")
+											.body(submitOrderJson)
+											.when()
+											.post("/api/order")
+											.then()
+											.log().all()
+											.statusCode(201)
+											.extract()
+											.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+	}
 }
